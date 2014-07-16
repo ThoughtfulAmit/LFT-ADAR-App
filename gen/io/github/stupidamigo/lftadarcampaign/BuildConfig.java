c19fa41565e204a84d66f273efc55de08de0582e
Amit Sharma <amit.sharma.tafs@gmail.com>
2014-07-16 07:55:19 +0530

    splash screen done

---
+++ gen/io/github/stupidamigo/lftadarcampaign/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package io.github.stupidamigo.lftadarcampaign;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/io/github/stupidamigo/lftadarcampaign/Splash.java
@@ -0,0 +1,41 @@
+package io.github.stupidamigo.lftadarcampaign;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class Splash extends Activity{
+
+	MediaPlayer ourSong;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(2000);					
+				} catch (InterruptedException e){
+					e.printStackTrace();
+				}finally{
+					Intent openStartingPoint = new Intent("io.github.stupidamigo.lftadarcampaign.USER");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+}
