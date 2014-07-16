e6f2250e2919d4f512e3ccb1cdcfa3e30fe09a65
Amit Sharma <amit.sharma.tafs@gmail.com>
2014-07-16 18:52:44 +0530

    scream sound and msg to one sender at a time

--- src/io/github/stupidamigo/lftadarcampaign/User.java
+++ src/io/github/stupidamigo/lftadarcampaign/User.java
@@ -3,24 +3,92 @@ package io.github.stupidamigo.lftadarcampaign;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.telephony.SmsManager;
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.os.Build;
 
 public class User extends ActionBarActivity {
+
+	Button bstart, bstop;
+	MediaPlayer scream;
+	Button sendBtn;
+	EditText txtphoneNo;
+	EditText txtMessage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user);
 
+		scream = MediaPlayer.create(User.this,
+				R.raw.stupid_amigo_deranged_women_scream);
+		bstart = (Button) findViewById(R.id.button1);
+		bstop = (Button) findViewById(R.id.button2);
+
+		scream.setLooping(true);
+		bstart.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				scream.start();
+			}
+
+		});
+
+		bstop.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				scream.pause();
+			}
+		});
+
+		sendBtn = (Button) findViewById(R.id.button4);
+		txtphoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
+		txtMessage = (EditText) findViewById(R.id.editTextSMS);
+
+		sendBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				sendSMSMessage();
+				txtphoneNo.setText("");
+				txtMessage.setText("");
+			}
+		});
+
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	protected void sendSMSMessage() {
+		Log.i("Send SMS", "");
+
+		String phoneNo = txtphoneNo.getText().toString();
+		String message = txtMessage.getText().toString();
+
+		try {
+			SmsManager smsManager = SmsManager.getDefault();
+			smsManager.sendTextMessage(phoneNo, null, message, null, null);
+			Toast.makeText(getApplicationContext(), "SMS sent.",
+					Toast.LENGTH_LONG).show();
+		} catch (Exception e) {
+			Toast.makeText(getApplicationContext(),
+					"SMS faild, please try again.", Toast.LENGTH_LONG).show();
+			e.printStackTrace();
 		}
 	}
 
@@ -59,6 +127,14 @@ public class User extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		scream.release();
+		finish();
 	}
 
 }
